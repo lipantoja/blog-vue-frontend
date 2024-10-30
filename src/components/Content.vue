a08d3eac69519f122b9d7d49033bf135ed28b4ca
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-10-29 20:40:25 -0400

    init w/ index

---
+++ src/components/Content.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from "axios";
+import PhotosIndex from "./PhotosIndex.vue";
+
+export default {
+  components: {
+    PhotosIndex,
+  },
+  data: function () {
+    return {
+      photos: [
+        { id: 1, name: "First", url: "https://via.placeholder.com/150", width: 150, height: 150 },
+        { id: 2, name: "Second", url: "https://via.placeholder.com/300", width: 300, height: 300 },
+      ],
+    };
+  },
+  created: function () {
+    this.handleIndexPhotos();
+  },
+  methods: {
+    handleIndexPhotos: function () {
+      axios.get("http://localhost:3000/photos.json").then((response) => {
+        console.log("photos index", response);
+        this.photos = response.data;
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <main>
+    <PhotosIndex v-bind:photos="photos"/>
+  </main>
+</template>
+
+<style></style>
---
+++ src/components/PhotosIndex.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    props: {
+      photos: Array,
+    },
+  };</script>
+
+<template>
+  <div>
+    <h1>All photos</h1>
+    <div v-for="photo in photos" v-bind:key="photo.id">
+      <h2>{{ photo.name }}</h2>
+      <img v-bind:src="photo.url" />
+      <p>Width: {{ photo.width }}</p>
+      <p>Height: {{ photo.height }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
